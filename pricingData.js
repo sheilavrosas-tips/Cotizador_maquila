4ab36df81514902c7ce2b1c1aa386eef2dd14aba
sheilavrosas-tips <sheila.v.rosas@gmail.com>
2026-06-25 11:15:08 -0600

    Primera version

---
+++ pricingData.js
@@ -0,0 +1,177 @@
+﻿const pricingData = [
+    {
+        "alcance":  "INCLUYE:\\nMedición de NER-NSCE\\nMedición de NPA-BANDAS DE OCTAVA Reconocimiento\\ninicial\\nElaboración de informe de resultados\\nPlanos de identificación de puntos de medición Certificados de calibración de equipos.\\n",
+        "category":  "Laboratorio",
+        "norm":  "NOM-011-STPS-2001 - ELABORACIÓN DE ESTUDIO",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "INCLUYE:\\nReconocimiento inicial\\nMedición de Nivel de Iluminación por personal calificado\\nMedición de Nivel de Reflexión\\nTres mediciones en caso iluminación artificial y natural\\nElaboración de informe de resultados\\nPlanos de identificación de puntos de medición\\nPlanos de distribución de luminarias Certificados de calibración de equipos\\n",
+        "category":  "Laboratorio",
+        "norm":  "NOM-025-STPS-2008 ELABORACIÓN DE ESTUDIO -Hasta 25 ptos-",
+        "priceMaquila":  12000,
+        "priceClient":  12000
+    },
+    {
+        "alcance":  "INCLUYE:\\nReconocimiento inicial\\nElaboración de informe de resultados\\nEstudio de agentes contaminantes, con resultado de ALTO (hasta 10 agentes).\\n",
+        "category":  "Estudio",
+        "norm":  "NOM-010-STPS-2014 RECONOCIMIENTO",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Condiciones térmicas elevadas-abatidas / Condiciones\\nde seguridad e higiene.\\nEmitido por laboratorio acreditado ante Entidad\\nMexicana de Acreditación y aprobado por secretaria del\\nTrabajo y Previsión Social.\\nINCLUYE:\\nReconocimiento inicial\\nElaboración de informe de resultados\\nPlanos de identificación de puntos de medición\\nCertificados de calibración de equipos\\nAcreditación ante Entidad Mexicana de Acreditación\\nUnidad de inspección\\nNOM-020-STPS-2011\\nRecipientes sujetos a presión\\nIncluye:\\nPlano del equipo en doble carta.\\nListado de equipo conforme a categoría.\\nExpediente de acuerdo a la categoría del equipo.\\nCroquis de localización.\\nDictamen.\\nPruebas de ensayos no destructivos.\\n- Ultrasonido industrial\\n- Líquidos penetrantes o Yugo magnético\\nUnidad de inspección\\nNOM-020-STPS-2011\\nGeneradores de vapor o calderas -\\nIncluye:\\nPlano del equipo en doble carta.\\nListado de equipo conforme a categoría.\\nExpediente de acuerdo a la categoría del equipo.\\nCroquis de localización.\\nDictamen.\\nPruebas de ensayos no destructivos.\\n- Ultrasonido industrial\\n- Líquidos penetrantes o Yugo magnético\\n",
+        "category":  "Laboratorio",
+        "norm":  "NOM-015-STPS-2001 ELABORACIÓN DE ESTUDIO",
+        "priceMaquila":  16000,
+        "priceClient":  9000
+    },
+    {
+        "alcance":  "Vibraciones-Condiciones de seguridad e higiene en los\\ncentros de trabajo.\\nEmitido por laboratorio acreditado ante Entidad\\nMexicana de Acreditación y Aprobado por Secretaría\\ndel Trabajo y Previsión Social.\\n",
+        "category":  "Laboratorio",
+        "norm":  "NOM-024-STPS-2001",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Electricidad estática en los centros de trabajo-\\nCondiciones de seguridad.\\nEmitido por Laboratorio Acreditado ante Entidad\\nMexicana de Acreditación y STPS.\\nINCLUYE:\\nTierra física\\nReconocimiento inicial\\nElaboración de informe de resultados\\nPlanos de identificación de puntos de medición\\nCertificados de calibración de equipos\\nAcreditación ante Entidad Mexicana de Acreditación\\nAprobación ante Secretaría del Trabajo y Previsión\\nSocial\\n",
+        "category":  "Laboratorio",
+        "norm":  "NOM-022-STPS-2015 ELABORACIÓN DE ESTUDIO",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Globally Harmonized System of Classification and Labelling of Chemical developed by the United Nations\\nElaboración de HDS con base en el sistema Globalmente Armonizado y en cumplimiento de la NOM-018-STPS-2015, numeral 9, y 9.2\\nIncluye:\\nEditable en Word\\nPdf en ingles\\nRequisitos:\\nListado de sustancias\\nPropiedades reológicas\\nPorcentaje de los componentes\\nHDS de los componentes\\n",
+        "category":  "Estudio",
+        "norm":  "Elaboración de MSDS bajo la clasificación:",
+        "priceMaquila":  2500,
+        "priceClient":  2500
+    },
+    {
+        "alcance":  "",
+        "category":  "Mannto",
+        "norm":  "MANTENIMIENTO A TIERRAS FÍSICAS Revisión de conexión mecánica Revisión de electrodo Limpieza dique Aplicación de Gem intensificador en caso de requerirse Pintura de marca 2000",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "",
+        "category":  "Laboratorio",
+        "norm":  "NOM-043-SEMARNAT-1993 - MEDIO AMBIENTE Monitoreo de Partículas Suspendidas Totales (PST\u0027s), conforme a las siguientes normas. (NMX-AA-009-SCFI-1993, NMX-AA-035-1976, NMX-AA054-1978, y NMX-AA-010-SCFI-2001) Para dar cumplimiento a la NOM-043-SEMARNAT-1993 ELABORACIÓN DE ESTUDIO Emitido por Laboratorio Acreditado ante Entidad Mexicana de Acreditación INCLUYE: Reconocimiento inicial Medición en campo Elaboración de informe de resultados Certificados de calibración de equipos 6500",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "",
+        "category":  "Laboratorio",
+        "norm":  "NOM-002-SEMARNAT-1996 Límites máximos permisibles de contaminantes en las descargas de aguas residuales a los sistemas de alcantarillado urbano o municipal. Análisis en laboratorio Informe de resultados Materia Flotante Ph (Promedio) Temperatura (Promedio) Demanda Bioquímica de oxígeno Clanuro Total Cromo Hexavalente Grasas y aceites (Promedio ponderado) Sólidos suspendidos totales Sólidos sedimentables Arsénico Cadmio Cobre Mercurio Níquel Plomo Zinc",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "INCLUYE:\\nReconocimiento inicial\\nElaboración de informe de resultados de cumplimiento de normas\\nRevisión de señaléticas erróneas\\nRecomendaciones\\n",
+        "category":  "Estudio",
+        "norm":  "NOM-026-STPS Colores y señaléticas",
+        "priceMaquila":  1850,
+        "priceClient":  1850
+    },
+    {
+        "alcance":  "Entregable:\\n\u0007 Informe de Normas aplicables que incluye nivel de cumplimiento por normas (en materia documental y visual)\\n\u0007 Informe de incidencias observadas por numeral no acreditado.\\n\u0007 Entrega de preliminar del software en sitio. (solo visual y a expensas de revisión de gabinete posteriores).\\n",
+        "category":  "Auditoria",
+        "norm":  "Diagnóstico tipo auditoría para conocer nivel de cumplimiento por norma, en materia de seguridad y salud ocupacional.",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Entregable:\\n\u0007 Informe de Aspectos evaluados aplicables que (en materia documental y visual)\\n\u0007 Informe de incidencias observadas no acreditadas.\\n\u0007 Entrega de preliminar del software en sitio. (solo visual y a expensas de revisión de gabinete posteriores).\\n",
+        "category":  "Auditoria",
+        "norm":  "Diagnóstico tipo auditoría para conocer nivel de cumplimiento en materia ambiental por aspectos: (aire, agua, suelo, residuos, ruido, energía, riesgo y emergencias)",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Contenido:\\n\u0007 Recorrido tecnico de las instalaciones de la empresa.\\n\u0007 Elaboracion y entrega de carpeta impresa y digital.\\n\u0007 Gestion de visto bueno ante la autoridad.\\n\u0007 Contestacion de observaciones en caso de ser necesario.\\n\u0007 Incluye carta de corresponsabilidad firmada por un tercer\\nacreditado la cual avala la elaboración del programa interno.\\n",
+        "category":  "Pc sureste",
+        "norm":  "Programa interno de proteccion civil",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Contenido:\\nCapacitación teórico-práctico de brigadas multifuncionales de emergencia en\\nmateria de protección civil para el personal de la empresa (Grupo máximo 15\\npersonas) en los siguientes rubros:\\n1) Primeros auxilios.\\n2) Prevención y combate de incendio.\\n3) Evacuación del inmueble.\\n4) Búsqueda y rescate.\\n\u0007 Entrega de constancias avaladas ante protección civil por tercer\\nacreditado registrado ante el municipio.\\n\u0007 Manual del participante digital.\\n\u0007 Duración de la capacitación de brigadas de emergencia máximo 8\\nhrs.\\n",
+        "category":  "Pc sureste",
+        "norm":  "Capacitación de brigadas de emergencia de pc",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "INCLUYE:\\nCharla de observación de\\nresultados y curso\\nergonomía y peligros [2 h]\\nHasta 11 dc3\\nVideos cortos generados\\ncon IA para observar los\\nmovimientos potenciales a\\ndaños [de lo puestos de\\ntrabajo más críticos]\\n[HASTA 11 PUESTO DE\\nTRABAJO].\\n",
+        "category":  "Estudio",
+        "norm":  "Análisis de los factores de riesgo ergonómico debido al manejo manual de cargas, con base en lo dispuesto por el Capítulo 7 de la NOM- 036-1-STPS-2018 Estimación simple del nivel de riesgo o evaluación rápida de las actividades identificadas. Evaluación específica del nivel de riesgo, cuando el resultado de la evaluación rápida no permita determinar el nivel de riesgo o condiciones aceptables.",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Programa de salud a los trabajadores expuestos al manejo de cargas que incluye:\\nAplicación de examen médico para integración de la historia clínica laboral.\\nDetección y análisis de los trabajadores que presentan signos o síntomas musculo-esqueléticos, aplicando el cuestionario Cuestionario Nórdico de Kuorinka (Nordic questionnaires for the analysis of musculoskeletal symptoms)\\nCertificado de aptitud física de los trabajadores que pueden manejar cargas.\\n",
+        "category":  "Estudio",
+        "norm":  "El programa para la vigilancia a la salud de los trabajadores ocupacionalmente expuestos, con lo dispuesto en el Capítulo 9.1 de la NOM-036-1-STPS-2018",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "",
+        "category":  "Estudio",
+        "norm":  "Procedimiento de seguridad para desarrollar las actividades que involucran el manejo manual de cargas, con lo dispuesto en el Capítulo 8.1 de la NOM-036-1-STPS-2018 Procedimiento de seguridad para desarrollar las actividades que involucran el manejo manual de cargas",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "Las medidas de control administrativas se deberán adoptar con el fin de proteger la salud del personal ocupacionalmente expuesto.\\nLas medidas de control técnicas por adoptar podrán\\n",
+        "category":  "Estudio",
+        "norm":  "El programa de ergonomía para el manejo manual de cargas, con lo dispuesto en el Capítulo 8.5 de la NOM-036-1-STPS-2018 Programa de ergonomía para el manejo manual de cargas.",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "INCLUYE:\\nVisita y recorrido a las instalaciones para determinar áreas y normas aplicables de las mismas.\\nReporte específico de normas aplicables, y puntos críticos por norma.\\n[HASTA 1 CENTRO DE TRABAJO].\\nCapacitación en línea sobre la misma norma para el RESPONSABLE DE SERVICIOS PREVENTIVOS\\n[hasta 3 participantes - incluye DC3]\\n",
+        "category":  "Estudio",
+        "norm":  "Diagnóstico de normas aplicables para el centro de trabajo con base en el numeral 4.3 de la NOM-030-STPS-2009",
+        "priceMaquila":  30000,
+        "priceClient":  37000
+    },
+    {
+        "alcance":  "- NOM-029-2011\\nINCLUYE:\\nVisita y recorrido para levantamiento.\\nENTREGABLES:\\nAnálisis de riesgos de peligros potenciales de las actividades de mantenimiento eléctrico.\\nDifusión de información\\nProgramas de mantenimiento\\nManual de primeros auxilios\\n",
+        "category":  "Estudio",
+        "norm":  "ELABORACIÓN DE CARPETA DE CUMPLIMIENTO PARA IMPLEMENTACIÓN DE LAS NORMAS:",
+        "priceMaquila":  15000,
+        "priceClient":  18500
+    },
+    {
+        "alcance":  "- NOM-004-STPS-1999\\nINCLUYE:\\nVisita y recorrido para levantamiento.\\nENTREGABLES:\\nAnálisis de riesgos de peligros potenciales de las máquinas.\\nDifusión de información\\nProgramas de mantenimiento\\nManual de primeros auxilios\\nPOR CARPETA\\n[Hasta 150 maquinarias]\\n",
+        "category":  "Estudio",
+        "norm":  "ELABORACIÓN DE CARPETA DE CUMPLIMIENTO PARA IMPLEMENTACIÓN DE LAS NORMAS:",
+        "priceMaquila":  0,
+        "priceClient":  0
+    },
+    {
+        "alcance":  "- NOM-027-STPS-\\nINCLUYE:\\nVisita y recorrido para levantamiento.\\nENTREGABLES:\\nAnálisis de riesgos de peligros potenciales de las actividades de corte y soldaduras.\\nDifusión de información\\nProgramas de mantenimiento\\nManual de primeros auxilios\\n[Hasta 20 actividades]\\n",
+        "category":  "Estudio",
+        "norm":  "ELABORACIÓN DE CARPETA DE CUMPLIMIENTO PARA IMPLEMENTACIÓN DE LAS NORMAS:",
+        "priceMaquila":  28000,
+        "priceClient":  28000
+    },
+    {
+        "alcance":  "- NOM-017-STPS-2024\\nINCLUYE:\\nVisita y recorrido para levantamiento.\\nENTREGABLES:\\nAnálisis de riesgos por puesto de trabajo\\nDeterminación de EPP requerido\\nProtocolo para reúso de equipos para visitantes\\nProtocolo para de limpieza para reuso de equipos para trabajadores\\nMaterial de difusión a los trabajadores\\nRequisitos de cumplimiento REPSE\\n[Hasta 35 puestos de trabajo]\\n",
+        "category":  "Estudio",
+        "norm":  "ELABORACIÓN DE CARPETA DE CUMPLIMIENTO PARA IMPLEMENTACIÓN DE LAS NORMAS:",
+        "priceMaquila":  38000,
+        "priceClient":  38000
+    },
+    {
+        "alcance":  "INCLUYE:\\nVisita y recorrido para levantamiento.\\nENTREGABLES:\\nAnálisis de riesgos de peligros\\nLas recomendaciones preventivas y/o correctivas para la administración de riesgos identificados.\\n",
+        "category":  "Estudio",
+        "norm":  "ESTUDIO DE NOM-028-STPS: análisis de riesgos de los procesos y equipos críticos que manejen sustancias químicas peligrosas, con la identificación, evaluación y recomendaciones preventivas y/o correctivas de los riesgos significativos, de conformidad con lo que prevé el Capítulo 7 de la presente Norma",
+        "priceMaquila":  0,
+        "priceClient":  0
+    }
+];
